dd2b36344c819b23d66f6df5edfa3f91cb9fbaa5
Michał Wielich <michal.wielich@adjust.com>
2020-05-21 20:50:48 +0200

    make it key & secret

--- app/services/lastfm_sync/download_artists.rb
+++ app/services/lastfm_sync/download_artists.rb
@@ -2,6 +2,7 @@ module LastfmSync
   class DownloadArtists
     def initialize(username)
       @username = username
+      @client = Lastfm.new(Settings.lastfm.api_key, Settings.lastfm.api_secret)
     end
 
     def download
